768dcf4213e1dc084183008cbc4ca37fe06b00b8
Max Alfaro <maxalfaro@Maxs-MacBook-Pro.local>
2019-05-14 15:41:11 -0700

    adding final version of sportsexclamation

---
+++ code/deploy_assignment/user_definition.py
@@ -0,0 +1,19 @@
+# ec2_address = "ec2-54-185-12-151.us-west-2.compute.amazonaws.com"
+# ec2_address = 'ec2-18-215-173-76.compute-1.amazonaws.com'
+
+# this is the main one
+ec2_address = 'ec2-54-210-187-72.compute-1.amazonaws.com'
+# this is jon-ross' personal one
+# ec2_address = 'ec2-54-244-69-138.us-west-2.compute.amazonaws.com'
+
+user = "ec2-user"
+# key_file = "/Desktop/MSDS/AWS/keys/oregon_keys.pem"
+key_file = '/Licenses/admin_key.pem'
+# key_file = '/Licenses/dajar88.pem'
+
+git_repo_owner = "MSDS698"
+git_repo_name = "product-analytics-group-project-sportsexclamation"
+git_user_id = "jrpresta"
+orig_coord = '37.7909,-122.3925'
+dest_coord = '37.7765,-122.4506'
+output_file_name = 'output.txt'
---
+++ documentation/build/html/_sources/tutorial.rst.txt
@@ -0,0 +1,4 @@
+Our Code Tutorial
+=================
+
+Run $python code/deploy_assignment/deploy.py on your terminal to update the server's code.
